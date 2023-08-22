e070034645241f0e7d16df08ea587c36973b4eee
seumomo <xo0207@naver.com>
2023-08-22 21:13:45 +0900

    [Add] React Router 환경 구성 및 Error Page 구현

--- mission03/src/main.jsx
+++ mission03/src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import './index.css';
+import ErrorPage from './pages/ErrorPage';
+import Landing from './pages/Landing.jsx';
+import GetBirthYear from './routes/GetBirthYear';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Landing />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: 'getbirthyear/submit',
+    element: <GetBirthYear />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ mission03/src/pages/ErrorPage.jsx
@@ -0,0 +1,20 @@
+import { useRouteError } from 'react-router-dom';
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div
+      id="error-page"
+      className="flex flex-col mx-auto mt-32 w-[600px] items-center gap-y-10">
+      <h1 className="text-red-500 text-8xl font-black">이런!</h1>
+      <p className="text-red-500 font-semibold text-xl">
+        죄송합니다. 예상치 못한 오류가 발생했습니다.
+      </p>
+      <p className="text-red-500 text-lg">
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
+}
