16abe1aa042ee195781c5ecbb8275b770a1c935b
seumomo <xo0207@naver.com>
2023-08-23 20:02:55 +0900

    [Feat] input value에 따른 button 활성화 여부 및 애니메이션 기능 구현

--- mission03/src/components/SubmitBirthYearForm.jsx
+++ mission03/src/components/SubmitBirthYearForm.jsx
@@ -1,26 +1,46 @@
-function SubmitBirthYearForm({ value }) {
+import { getCurrentYear } from "../utils";
+
+function SubmitBirthYearForm({ value, onChange }) {
+  const isFull = value.length === 4;
+  const isDisable = () => {
+    return isFull ? false : true;
+  };
+
   return (
     <form className="flex flex-col items-center gap-y-10">
       <label htmlFor="getBirthYear" className="text-5xl">
-        언제 태어났나요?
+        몇 년도에 태어나셨나요?
       </label>
+
       <div className="text-3xl">
         🎉{" "}
         <input
-          type="text"
+          type="number"
           id="getBirthYear"
           name="getBirthYear"
-          className="h-14 w-64 rounded-full border-2 border-red-500 text-center transition-all focus:w-36 focus:outline-green-500 "
+          className="m-0 h-14 w-64 rounded-full border-2 border-red-500 text-center transition-all focus:w-36 focus:outline-green-500 focus:placeholder:text-transparent"
           required
           placeholder="출생년도 4자리"
           minLength={4}
           maxLength={4}
           title="출생년도 4자리를 입력해 주세요."
           defaultValue={value}
+          onChange={onChange}
         />
-        <button type="submit" className="font-bold">
+        <button
+          type="submit"
+          className={`disabled: ml-4 font-bold hover:text-orange-500 disabled:text-black`}
+          disabled={isDisable()}
+        >
           년에 태어났어요 🎉
         </button>
+        <p
+          className={`relative right-0 ${
+            isFull ? "visible -left-11" : "invisible left-[9999px]"
+          } -top-3 text-right text-lg text-green-500 transition-all duration-500`}
+        >
+          👆 여기를 눌러주세요! 👆
+        </p>
       </div>
     </form>
   );
--- mission03/src/routes/GetBirthYear.jsx
+++ mission03/src/routes/GetBirthYear.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react';
-import SubmitBirthYearForm from '../components/SubmitBirthYearForm';
+import { useState } from "react";
+import SubmitBirthYearForm from "../components/SubmitBirthYearForm";
+import debounce from "./../utils/debounce";
 
 function GetBirthYear() {
-  const [inputYear, setInputYear] = useState('');
+  const [yearValue, setYearValue] = useState("");
+  const handleChangeYearValue = debounce((e) => {
+    setYearValue(e.target.value);
+  });
 
   return (
     <div className="flex flex-col gap-y-14">
-      <SubmitBirthYearForm value={inputYear} />
+      <SubmitBirthYearForm value={yearValue} onChange={handleChangeYearValue} />
     </div>
   );
 }
