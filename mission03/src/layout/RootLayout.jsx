f11951e2b68feb0d39750459d9ca6aac146627c2
seumomo <xo0207@naver.com>
2023-08-23 16:18:02 +0900

    [Add] favicon 추가 및 SUIT 폰트 적용

--- mission03/src/layout/RootLayout.jsx
+++ mission03/src/layout/RootLayout.jsx
@@ -6,9 +6,11 @@ function RootLayout() {
   return (
     <div className="mt-28 min-w-[768px] flex flex-col gap-y-16">
       <Header />
+
       <main>
         <Outlet />
       </main>
+
       <Footer />
     </div>
   );
